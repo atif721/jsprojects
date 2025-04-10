9adf222daa3a1389227277fc31ee46a1e6012b07
Abdullah Atif <abdullahatif721@outlook.com>
2025-04-10 12:07:15 +0600

    c

--- 1TodoApp/src/components/WelcomeMesg.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./WelcomeMesg.module.css";
-
-function WelcomeMsg() {
-  return <p className={`${styles.welmsg}`}>All Tasks Completed.Enjoy your Day</p>;
-}
-
-export default WelcomeMsg;
