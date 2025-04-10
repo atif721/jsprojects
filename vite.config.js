0f9e79f45c26ad2f3405a2c0fe712d0080e81572
Abdullah Atif <abdullahatif721@outlook.com>
2025-04-10 18:59:25 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ReactProjects/001TodoApp/", // <-- important
   plugins: [react()],
-})
+});
